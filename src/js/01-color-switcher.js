f8fd3a191034d76bd7469ba0e18f931812c518fc
nadtosonna <prasolova.kateryna@gmail.com>
2022-07-20 00:13:55 +0300

    task-1 added, version to further rework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+let timerId = null;
 
+refs.startBtn.addEventListener('click', changeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function changeColor() {
+    timerId = setInterval(() => {
+        let color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+}, 1000);
+}
+function stopChangeColor() {
+    clearInterval(timerId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
