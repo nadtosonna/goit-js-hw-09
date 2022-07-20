9b0a051748ed80ad8d9679f2214ca48b5ad05fee
nadtosonna <97362066+nadtosonna@users.noreply.github.com>
2022-07-20 15:26:39 +0000

    Deploying to gh-pages from @ nadtosonna/goit-js-hw-09@9f6130f4ea1594a747ea4b63a6e433bfadfd4f54 🚀

--- 01-color-switcher.49a8cb47.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;t.startBtn.addEventListener("click",(function(){n=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.49a8cb47.js.map
